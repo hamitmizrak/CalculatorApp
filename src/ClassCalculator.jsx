8123d2f76ecb06b243e3c67b553a1f203b39738a
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-25 12:52:24 +0300

    Calculator

--- src/ClassCalculator.jsx
+++ src/ClassCalculator.jsx
@@ -19,7 +19,7 @@ class ClassCalculator extends Component {
       inputData1: "",
       inputData2: "",
       result: "",
-      date: "25/09/2023",
+      date: new Date().getFullYear(), // "25/09/2023",
     }
     // BIND
     this.calculatorSum = this.calculatorSum.bind(this);
@@ -78,7 +78,7 @@ class ClassCalculator extends Component {
 
     // SETSTATE
     this.setState({
-      result: resultValue,
+      result: "2025",
     });
   }
 
@@ -95,10 +95,22 @@ class ClassCalculator extends Component {
     })
   }
 
+  // CDM
+  componentDidMount() { 
+    this.nowDate();
+   }
+
+  // FUNCTION
   // Şimdiki tarihi göster
   nowDate() {
-    const date = new Date().getDate();
-    // SetState
+    // const temp=new Date().getDate()+" / "+(new Date().getMonth()+1)+" / "+new Date().getFullYear();
+    const temp=`${new Date().getDate()} / ${(new Date().getMonth()+1)} / ${new Date().getFullYear()}  `;
+
+    //SET STATE
+    this.setState({
+      // date:temp
+      date:`${temp}`
+    })
   }
 
   // RENDER
@@ -172,7 +184,7 @@ class ClassCalculator extends Component {
             color="purple"
             title="%"
           />
-           <Text style={styles.calculatorResultText}>Sonuç: {this.state.result}</Text>
+          <Text style={styles.calculatorResultText}>Sonuç: {this.state.result}</Text>
         </View>
       </View>
     ) //end return
@@ -187,32 +199,32 @@ const styles = StyleSheet.create({
     // alignItems: 'center',     // satır düzleminde (X)
     // justifyContent: 'center', // sutun düzleminde (Y)
     // flexDirection: 'column'
-    flexDirection:"column",
+    flexDirection: "column",
   },
   calculatorContainer: {
-    padding:40,
-    flexDirection:"column",
-    justifyContent:"center",
+    padding: 25,
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: 'center',
-    
+
   },
   calculatorButton: {
     borderRadius: 5,
-    margin:10,
+    margin: 5,
   },
   calculatorInput: {
-    height:50,
-    backgroundColor:"white",
-    color:"blue",
-    width:380,
-    margin:10,
-    padding:10
+    height: 50,
+    backgroundColor: "white",
+    color: "blue",
+    width: 380,
+    margin: 5,
+    padding: 5
   },
-  calculatorResultText:{
-    color:"black",
-    fontSize:20,
-    fontWeight:"bold",
-    marginTop:30
+  calculatorResultText: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20
   }
 
 });
